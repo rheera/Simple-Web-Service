724c45d3aa0b7702e9171ae2662c813a930c8b6b
Raman Heera <rheera2@uwo.ca>
2019-04-08 12:54:03 -0400

    Update Customer.java
    - added some attributes and added Entity from lombok to generate getters and setters

--- src/main/java/com/rfws/firstwebservice/domain/Customer.java
+++ src/main/java/com/rfws/firstwebservice/domain/Customer.java
@@ -1,4 +1,21 @@
 package com.rfws.firstwebservice.domain;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+//From project lombok, helps you easily set up getters and setters
+// Make sure preferences-> Build, execution, deployment -> Compiler -> Annotation Processors -> Enable Annotation Processing is checked
+@Data
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+
 }
